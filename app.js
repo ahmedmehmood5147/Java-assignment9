de3b6302e79e44451d217ab6758e037ca0cc9d04
ahmedmehmood5147 <am9474036@gmail.com>
2023-11-19 05:09:47 +0500

    java-assignment

---
+++ app.js
@@ -0,0 +1,21 @@
+var std = ["ali","ahmed","muzammil"]
+console.log(std)
+var std1 = [1 , 2 ,3 , 4 , 5]
+console.log(std1);
+var std1 = [true , false]
+console.log (std1)
+var mixedArray = ["ali","ahmed", 1 , 6 , false , true]
+console.log (mixedArray)
+var qualification = ["ssd" ,"<br>" , "BCOM"  , "<br>" , "Inter" , "<br>" , "Matric"]
+document.write (qualification)
+var student = ["Ahmed" , "Ali" , "Muzammil"]
+var studentScore = [320 , 230 , 480]
+var totalMarks = 500
+document.write ("<br>" + "Score of ahmed is" + " " + studentScore[0] + " " + "Percentage:" + " " + studentScore[0] / totalMarks * 100)
+document.write ("<br>" + "Score of ali is" + " " + studentScore[1] + " " + "Percentage:" + " " + studentScore[1] / totalMarks * 100)
+document.write ("<br>" + "Score of ahmed is" + " " + studentScore[2] + " " + "Percentage:" + " " + studentScore[2] / totalMarks * 100)
+var colour = ["<br>" + "Orange" , "Purple" , "White" , "Green" , " "]
+document.write (colour)
+var userName = prompt ("What is your favourite colour")
+colour.unshift (userName)
+document.write (userName)
